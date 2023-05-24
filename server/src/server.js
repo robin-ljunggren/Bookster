67e7b5de8dd081bfe51759c2ac3412371c6abf61
j-carlander <johannes.carlander@gmail.com>
2023-05-24 10:19:05 +0200

    changed servoer port to be 4000

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import cors from 'cors';
-import authRouter from './router/authRouter.js';
-import bookRouter from './router/bookRouter.js';
-import adminRouter from './router/adminRouter.js';
-import guestRouter from './router/guestRouter.js';
-import authFormatFilter from './middleware/authFormatFilter.js';
-import jwtFilter from './middleware/jwtFilter.js';
-import profileController from './controller/profileController.js';
+import express from "express";
+import cors from "cors";
+import authRouter from "./router/authRouter.js";
+import bookRouter from "./router/bookRouter.js";
+import adminRouter from "./router/adminRouter.js";
+import guestRouter from "./router/guestRouter.js";
+import authFormatFilter from "./middleware/authFormatFilter.js";
+import jwtFilter from "./middleware/jwtFilter.js";
+import profileController from "./controller/profileController.js";
 
-const port = 3000;
+const port = 4000;
 
 const app = express();
 
@@ -17,9 +17,16 @@ app.use(express.json());
 
 app.use("/library", guestRouter);
 app.use("/auth", authFormatFilter, authRouter);
-app.use("/library/profile", jwtFilter.bind({role: ["USER", "ADMIN"]}), profileController.getProfile);
-app.use("/library/user", jwtFilter.bind({role: ["USER", "ADMIN"]}), bookRouter);
-app.use("/admin", jwtFilter.bind({role: "ADMIN"}), adminRouter);
-
+app.use(
+  "/library/profile",
+  jwtFilter.bind({ role: ["USER", "ADMIN"] }),
+  profileController.getProfile
+);
+app.use(
+  "/library/user",
+  jwtFilter.bind({ role: ["USER", "ADMIN"] }),
+  bookRouter
+);
+app.use("/admin", jwtFilter.bind({ role: "ADMIN" }), adminRouter);
 
 app.listen(port, () => console.log("Server started on port: " + port));
