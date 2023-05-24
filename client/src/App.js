34db10d7f2c6d3f82fe2eecb138662316431c5d5
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-05-24 14:13:39 +0200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <table>
-      <thead>
-        <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        </tr>
-      </thead>
-    </table>
-  );
+  
 }
 
 export default App;
