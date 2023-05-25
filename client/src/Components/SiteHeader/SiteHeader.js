a8f173908c649fab10707f97401b990f7a6b3001
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-05-25 09:00:25 +0200

    changes done to site header component

--- client/src/Components/SiteHeader/SiteHeader.js
+++ client/src/Components/SiteHeader/SiteHeader.js
@@ -4,26 +4,17 @@ import './SiteHeader.css';
 export default function SiteHeader() {
 
   // states and username will be passed through useContext instead
-  const [userState, setUserState] = useState('guest');
-  const [loginState, setLoginState] = useState(true);
-  let username = "user";
+  const [loggedInState, setLoggedInState] = useState(false);
+  let username = "Bob";
 
   return (
-    <div>
-      {loginState && (
-        <header className='site-header-container-login'>
-          <h1 className='site-header-h1-login'>Booksters Website</h1>
-        </header>
-      )};
-      {!loginState && (
-        <header className='site-header-container'>
-          <h1 className='site-header-h1'>Booksters Website</h1>
-          <section className='site-header-section'>
-            <p>{userState === 'guest' ? `Browsing as guest...` : `Browsing as user ${username}`}</p>
-            <button>{userState === 'guest' ? "Sign in" : "Sign out"}</button>
-          </section>
-        </header>
-      )};
-    </div>
+
+    <header className='site-header-container'>
+      <h1 className='site-header-h1'>Booksters Website</h1>
+      <section className='site-header-section'>
+        <p>{!loggedInState ? `Browsing as guest...` : `Browsing as user ${username}`}</p>
+        <button>{!loggedInState ? "Sign in" : "Sign out"}</button>
+      </section>
+    </header>
   )
 }
