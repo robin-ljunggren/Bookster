e31c2ddcb118ea2677106e362bbf76eb76e22f00
j-carlander <johannes.carlander@gmail.com>
2023-05-29 08:27:41 +0200

    fix in servercode

--- server/src/service/bookService.js
+++ server/src/service/bookService.js
@@ -1,28 +1,30 @@
-import bookRepository from "../repository/bookRepository.js"
+import bookRepository from "../repository/bookRepository.js";
 import userRepository from "../repository/userRepository.js";
 
 const purchaseBook = (username, title, quantity) => {
   const book = bookRepository.getBook(title);
 
-  if(book === undefined) {
+  if (book === undefined) {
     let error = new Error("Title match not found");
     error.name = "BookTitleNotFoundException";
     throw error;
   }
 
-  if(book.quantity < quantity) {
+  if (book.quantity < quantity) {
     let error = new Error("Not enough quantity");
     error.name = "MissingQuantityException";
     throw error;
   }
 
-  userRepository.addPurchase(username, book);
-  return bookRepository.patchBook({...book, quantity: book.quantity - quantity});
-}
+  userRepository.addPurchase(username, { ...book, quantity: quantity });
+  return bookRepository.patchBook({
+    ...book,
+    quantity: book.quantity - quantity,
+  });
+};
 
 const viewAll = () => bookRepository.getBooks();
 
 const searchBook = (query) => bookRepository.searchBooks(query);
-
 
 export default { purchaseBook, viewAll, searchBook };
