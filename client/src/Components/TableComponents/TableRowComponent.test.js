2ece3fe2a22473245fb7a5ad5f8f51b6de85f3bc
j-carlander <johannes.carlander@gmail.com>
2023-06-01 15:45:01 +0200

    changed test for orderbook to test if all button are disabled if book is out of stock

--- client/src/Components/TableComponents/TableRowComponent.test.js
+++ client/src/Components/TableComponents/TableRowComponent.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import TableRowComponent from "./TableRowComponent";
 
-test("that a book renders in the table ", () => {
+test("that a book renders in the table", () => {
   const books = [
     {
       title: "Harry Potter",
